34d12c212a6c152990fd543425a0e3e87a10fedc
Olga Kolyachko <olga@alan.app>
2021-06-03 15:55:02 +0300

    new theme

--- _static/scripts/custom.js
+++ _static/scripts/custom.js
@@ -1,31 +1,11 @@
 //Custom JS
 
-//Clear cache for select elements
- 
-$(document).ready(function () {
-    $("select").each(function () {
-        $(this).val($(this).find('option[selected]').val());
-    });
-});
 
 // Fixing sticky header error
 
 $(document).ready(function () {
     $(".headerlink").parent().addClass("anchor-fixed");
 });
-
-
-//API
-
-$('table.api-code-block  td').html(function (i, html) {
-	return html.replace(/&nbsp;/g, '');
-});
-
-
-//Logo
-
-$('.site-title').text(' ');
-$('.site-title').addClass('logo-img');
 
 
 
