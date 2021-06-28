2aaf942884a5b35f93e4dffbc497b5fd2cc5126a
Olga Kolyachko <olga@alan.app>
2021-06-28 16:08:51 +0300

    alternative builder

--- _static/scripts/custom.js
+++ _static/scripts/custom.js
@@ -1,11 +1,23 @@
 //Custom JS
 
-
 // Fixing sticky header error
 
 $(document).ready(function () {
     $(".headerlink").parent().addClass("anchor-fixed");
 });
 
+// Adding target to external links
 
+$(document).ready(function () {
+	$('a[href^="http://"], a[href^="https://"]').not('a[class*=internal]').attr('target', '_blank');
+});
+
+// Expanding dropdowns on ctrl + f clicked
+
+$(document).keydown(function(e) {
+        if (e.keyCode == 70 && e.ctrlKey) {
+            $(".dropdown").prop("open", true);
+        }
+    });
+	
 
