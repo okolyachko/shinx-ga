b3e8edbfccc205d716efdd434e7e6ae1364b2c57
Olga Kolyachko <olga@alan.app>
2021-08-04 17:57:05 +0300

    search testing

---
+++ _static/assets/img/tutorial2/ts-2.js
@@ -0,0 +1,46 @@
+// menu items
+const items = ['pepperoni', 'margherita', 'burrito',
+    'burger', 'taco', 'apple pie'];
+
+// itemList = "pepperoni_|margherita_|burrito_|burger_|taco_|apple pie_";
+const itemList = items.map(i => i + '_').join('|');
+
+// add items to order
+intent(`(Add|I want|) $(ITEM ${itemList})`,
+       `(Add|I want|) $(NUMBER) $(ITEM ${itemList})`, p => {
+    let number = p.NUMBER ? p.NUMBER.number : 1;
+    const item = items.find(i => p.ITEM.startsWith(i));
+    p.play({
+        command: 'changeOrder',
+        item: item,
+        quantity: number
+    });
+    p.play(`Adding ${number} ${p.ITEM.value}`);
+});
+
+// remove or update order items 
+follow(`(Remove|delete) $(ITEM ${itemList})`,
+       `(Remove|delete) $(NUMBER) $(ITEM ${itemList})`, p => {
+    const item = items.find(i => p.ITEM.startsWith(i));
+    let deleteQnty = p.NUMBER ? p.NUMBER.number : 1;
+
+    p.play('Removing ' + p.ITEM.value);
+    p.play({
+        command: 'changeOrder',
+        item: item,
+        quantity: -deleteQnty
+    });
+});
+
+var whatAddress = context(() => {
+    follow('$(LOC)', p => {
+        p.play({command: "address",
+                address: p.LOC.value});
+        p.play("We will deliver your order to " + p.LOC.value);
+    });
+});
+
+intent(`That's (all|it)`, '(Ready to|) checkout', p => {
+    p.play('What is delivery address?');
+    p.then(whatAddress);
+});
